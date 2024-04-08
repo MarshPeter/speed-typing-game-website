895117095d546660ace1e4ce86bba5af24f32a68
MarshPeter <peter.m.farmer.204@gmail.com>
2024-04-08 22:59:15 +1000

    can now calculate words per minute

--- src/components/TypingPrompt.tsx
+++ src/components/TypingPrompt.tsx
@@ -22,42 +22,58 @@ export default function TypingPrompt({
     const phraseLength = phrase.length;
 
     const [correctChracterArray, setCorrectCharacterArray] = useState((new Array(phraseLength)).fill(0));
+    const [wordsPerMinute, setWordsPerMinute] = useState(0);
+    const [wordsComplete, setWordsComplete] = useState(0);
+    const [startTime, setStartTime] = useState(performance.now());
+    // const startTime = performance.now();
 
-    useEffect(() => {
-      function handleKeyDown(e: any) {
-        if (highlightIndex > phrase.length) return;
-        const key = e.key;
-        console.log(key);
-        if (key.toLowerCase() === "shift" || key.toLowerCase() === "capslock") return;
-        let correct = currentCorrect;
-        if (phrase[highlightIndex] === key) {
-          setCurrentCorrect(currentCorrect + 1);
-          correct++;
-        }
-        const newCorrectArray = correctChracterArray.map((val: number, index: number) => {
-          if (index !== highlightIndex) return val;
-
-          if (phrase[highlightIndex] === key) {
-            return 1;
-          } else {
-            return -1;
-          }
-        })
-
-        console.log(correctChracterArray);
-
-        setCurrentAverage((correct / (highlightIndex + 1)) * 100);
-        setHighlightIndex(highlightIndex + 1);
-        setCorrectCharacterArray(newCorrectArray);
+    function handleKeyDown(e: any) {
+      if (highlightIndex > phrase.length) return;
+      const key = e.key;
+      console.log(key);
+      if (key.toLowerCase() === "shift" || key.toLowerCase() === "capslock") return;
+      let correct = currentCorrect;
+      if (phrase[highlightIndex] === key) {
+        setCurrentCorrect(currentCorrect + 1);
+        correct++;
       }
 
-      document.addEventListener('keydown', handleKeyDown);
+      const newCorrectArray = correctChracterArray.map((val: number, index: number) => {
+        if (index !== highlightIndex) return val;
 
+        if (phrase[highlightIndex] === key) {
+          return 1;
+        } else {
+          return -1;
+        }
+      })
+
+      if (phrase[highlightIndex] === " ") {
+        setWordsComplete(wordsComplete + 1);
+      }
+
+      setCurrentAverage((correct / (highlightIndex + 1)) * 100);
+      setHighlightIndex(highlightIndex + 1);
+      setCorrectCharacterArray(newCorrectArray);
+      calculateWordsPerMinute();
+    }
+
+    function calculateWordsPerMinute() {
+      const currentTime = performance.now();
+      const timeDifference = currentTime - startTime;
+      const timeDifferenceInMinutes = timeDifference / (1000 * 60);
+      const currentWordsPerMinute = (wordsComplete / timeDifferenceInMinutes);
+      setWordsPerMinute(currentWordsPerMinute);
+    }
+
+    useEffect(() => {
+
+      document.addEventListener('keydown', handleKeyDown);
       return () => {
         document.removeEventListener('keydown', handleKeyDown);
       };
 
-    }, [highlightIndex, currentCorrect]);
+    }, [highlightIndex, currentCorrect, correctChracterArray, wordsPerMinute, wordsComplete]);
 
     return (
       <div className="flex flex-col gap-4 p-5 rounded border-8 border-indigo-400">
@@ -82,7 +98,8 @@ export default function TypingPrompt({
         </p>
         <div className="flex justify-between text-lg">
           <p>Correct: {currentCorrect} / {phraseLength}</p>
-          <p>Average: {currentAverage}%</p>
+          <p>WPM: {wordsPerMinute.toFixed(2)}</p>
+          <p>Average: {currentAverage.toFixed(0)}%</p>
         </div>
       </div>
     )
