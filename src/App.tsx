308aa288044d4d8c7764c63f112e12856c815fb8
MarshPeter <peter.m.farmer.204@gmail.com>
2024-04-18 19:53:08 +1000

    App cleaned up

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,5 @@
-import { useEffect, useState } from "react";
-import textPromptFetcher from "./services/TextPromptFetcher";
-import Header from "./components/Header";
-import TypingPrompt from "./components/TypingPrompt";
-import Button from "./components/Button";
-
 function App() {
-  const [phrase, setPhrase] = useState("");
-  const [highlightIndex, setHighlightIndex] = useState(0);
-  const [currentCorrect, setCurrentCorrect] = useState(0);
-  const [currentAverage, setCurrentAverage] = useState(0);
-  const [showPrompt, setShowPrompt] = useState(false);
-
-  function setPhrasePrompt(prompt: (string | null)) {
-    if (prompt === null) {
-      setPhrase(textPromptFetcher.getPrompt());
-    } else {
-      setPhrase(prompt);
-    }
-
-    changePromptStartState();
-    return;
-  }
-
-  function changePromptStartState() {
-    setShowPrompt(!showPrompt);
-  }
-
-  return (
-    <div className="bg-gray-200 min-h-screen min-w-screen">
-      <Header></Header>
-      <main className="flex justify-center">
-        <div className="flex w-2/3">
-          {showPrompt ?
-            <TypingPrompt
-              phrase={phrase}
-              highlightIndex={highlightIndex}
-              setHighlightIndex={setHighlightIndex}
-              currentCorrect={currentCorrect}
-              setCurrentCorrect={setCurrentCorrect}
-              currentAverage={currentAverage}
-              setCurrentAverage={setCurrentAverage}
-              setShowPrompt={setShowPrompt}
-            /> :
-            <Button onPressFunction={setPhrasePrompt} prompt={"Start Typing Test!"} />
-          }
-        </div>
-      </main>
-    </div>
-  )
+    return <div>Hello World</div>;
 }
 
-export default App
+export default App;
